a7807be8d9de5e7816703bcbb0ce5bf308c92d28
Humza Khan <Humzakhan071@gmail.com>
2021-10-02 22:00:34 +0500

    Story_App_UI

---
+++ lib/data.dart
@@ -0,0 +1,14 @@
+List<String> images=[
+  "assets/image_01.png",
+  "assets/image_02.png",
+  "assets/image_03.png",
+  "assets/image_04.png",
+];
+
+List<String> title=[
+  "Hounted Ground",
+  "Fallen in Love",
+  "The Dreaming Hoon",
+  "Jack the Persian and the Black Costel",
+
+];
